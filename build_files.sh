93923404cb9686607ac20e107c7b9009de1ecf62
Mrutunjaya-Muduli <mrutunjayamuduli54@gmail.com>
2026-07-24 23:24:40 +0530

    Fix Vercel build by using virtualenv in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,15 @@
 # build_files.sh
 echo "BUILD START"
 
-# Install requirements
-python3 -m pip install -r requirements.txt
+# Create a temporary virtual environment for the build phase
+python3 -m venv venv_build
+source venv_build/bin/activate
+
+# Install requirements inside the virtual environment
+pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
+
